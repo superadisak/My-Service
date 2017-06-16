bdbf9e84af496325bf13b5e6d976df5301b1f0b5
Student18 <axiom2517@gmail.com>
2017-06-16 10:43:23 +0700

    upload new user

--- My Service/ViewController.swift
+++ My Service/ViewController.swift
@@ -5,7 +5,6 @@
 //  Created by Teacher on 6/15/2560 BE.
 //  Copyright © 2560 SNRU. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,31 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Process Connected Http
-        let urlPHP = "http://swiftcodingthai.com"
-        //Change String to url String
-        let myURL = URL(string: urlPHP)
-        
-        let request = NSMutableURLRequest(url: myURL!)
-        let task = URLSession.shared.dataTask(with: request as URLRequest){
-            data, response, error in
-            
-            if error != nil {
-                print("Error ==> \(error)")
-            }   else {
-                
-                if let unwrappedData = data {
-                    
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                    let strJSON = dataString as Any
-                    print("strJSON ==> \(strJSON)")
-                    
-                }
-                
-            }   // if1
-            
-        }
-        task.resume()
+//        //Process Connected Http
+//        let urlPHP = "http://androidthai.in.th/fetl/getUserMaster.php"
+//        //Change String to url String
+//        let myURL = URL(string: urlPHP)
+//        
+//        let request = NSMutableURLRequest(url: myURL!)
+//        let task = URLSession.shared.dataTask(with: request as URLRequest){
+//            data, response, error in
+//            
+//            if error != nil {
+//                print("Error ==> \(error)")
+//            }   else {
+//                
+//                if let unwrappedData = data {
+//                    
+//                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+//                    let strJSON = dataString as Any
+//                    print("strJSON ==> \(strJSON)")
+//                    
+//                }
+//                
+//            }   // if1
+//            
+//        }
+//        task.resume()
         
         
         
@@ -46,11 +45,9 @@ class ViewController: UIViewController {
         
         
     }   // Main Method
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }   // didReceive
-    
     
 }   // Main Class
